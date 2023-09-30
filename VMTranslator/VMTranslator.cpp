4611bd764987770f85d3ba7241b44f93729961d2
Felix <a1878510@adelaide.edu.au>
2023-09-30 22:12:07 +0930

    Update Prac6 Task4

--- VMTranslator/VMTranslator.cpp
+++ VMTranslator/VMTranslator.cpp
@@ -19,46 +19,32 @@ VMTranslator::~VMTranslator() {
 }
 
 /** Generate Hack Assembly code for a VM push operation */
-string VMTranslator::vm_push(string segment, int offset) {
-    string asm_code;
-    if (segment == "constant") {  // Handling constant values
-      asm_code = "@" + to_string(offset) + "\n" +
-                 "D=A\n";  // load constant into D register
-    } else {
-      // Handle other segments like local, argument, this, that, etc.
-      string segment_base;
-      if (segment == "local")
-        segment_base = "LCL";
-      else if (segment == "argument")
-        segment_base = "ARG";
-      // ... handle other segments
-      asm_code = "@" + segment_base + "\n" + "D=M\n" + "@" + to_string(offset) +
-                 "\n" + "A=D+A\n" +  // compute the effective address
-                 "D=M\n";            // load value into D register
-    }
-    asm_code += "@SP\n" + string("A=M\n") +
-                "M=D\n" +             // push D register value onto the stack
-                "@SP\n" + "M=M+1\n";  // increment stack pointer
-    return asm_code;
+string VMTranslator::vm_push(string segment, int index) {
+    string asmCode;
+    if (segment == "constant") {
+      asmCode = "@" + to_string(index) + "\n"  // set A to index
+                + "D=A\n"                      // D = A
+                + "@SP\n" + "A=M\n"            // go to the top of the stack
+                + "M=D\n"                      // set the top of the stack to D
+                + "@SP\n" + "M=M+1\n";         // increment the stack pointer
+    } else if (segment == "local") {
+      // Handle local segment push
+      asmCode = "@" + to_string(index) + "\n" + "D=A\n" + "@LCL\n" + "A=D+M\n" +
+                "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" + "@SP\n" + "M=M+1\n";
+    }  // Add additional else if statements for other segments
+    return asmCode;
 }
 
 /** Generate Hack Assembly code for a VM pop operation */
-string VMTranslator::vm_pop(string segment, int offset) {
-    string asm_code, segment_base;
-    if (segment == "local")
-      segment_base = "LCL";
-    else if (segment == "argument")
-      segment_base = "ARG";
-    // ... handle other segments
-
-    asm_code = "@" + segment_base + "\n" + "D=M\n" + "@" + to_string(offset) +
-               "\n" + "D=D+A\n" +      // compute the effective address
-               "@R13\n" + "M=D\n" +    // store the effective address in R13
-               "@SP\n" + "AM=M-1\n" +  // decrement stack pointer and load top
-                                       // of stack into D register
-               "D=M\n" + "@R13\n" + "A=M\n" +
-               "M=D\n";  // store D register value at the effective address
-    return asm_code;
+string VMTranslator::vm_pop(string segment, int index) {
+    string asmCode;
+    if (segment == "local") {
+      // Handle local segment pop
+      asmCode = "@SP\n" + "M=M-1\n" + "A=M\n" + "D=M\n" + "@" +
+                to_string(index) + "\n" + "D=D+A\n" + "@LCL\n" + "A=D+M\n" +
+                "M=D\n";
+    }  // Add additional else if statements for other segments
+    return asmCode;
 }
 
 /** Generate Hack Assembly code for a VM add operation */
@@ -128,84 +114,72 @@ string VMTranslator::vm_not() { return "@SP\n" + string("A=M-1\n") + "M=!M\n";
 }
 
 /** Generate Hack Assembly code for a VM label operation */
-string VMTranslator::vm_label(string label) { return "(" + label + ")\n"; }
+string VMTranslator::vm_label(string labelName) {
+    return "(" + labelName + ")\n";
+}
 
 /** Generate Hack Assembly code for a VM goto operation */
-string VMTranslator::vm_goto(string label){
-    return "@" + label + "\n" + "0;JMP\n";
+string VMTranslator::vm_goto(string labelName) {
+    return "@" + labelName + "\n" + "0;JMP\n";
 }
 
 /** Generate Hack Assembly code for a VM if-goto operation */
-string VMTranslator::vm_if(string label){
-    return "@SP\n" + string("AM=M-1\n") + "D=M\n" + "@" + label + "\n" + "D;JNE\n";
+string VMTranslator::vm_if(string labelName) {
+    return "@SP\n" + "AM=M-1\n" + "D=M\n" + "@" + labelName + "\n" + "D;JNE\n";
 }
 
 /** Generate Hack Assembly code for a VM function operation */
-string VMTranslator::vm_function(string function_name, int n_vars){
-    string asm_code = "(" + function_name + ")\n";
-    for (int i = 0; i < n_vars; i++) {
-      asm_code += "@SP\n" + string("A=M\n") + "M=0\n" + "@SP\n" + "M=M+1\n";
+string VMTranslator::vm_function(string functionName, int numLocals) {
+    string asmCode =
+        "(" + functionName + ")\n";  // Label for the function entry
+    for (int i = 0; i < numLocals; i++) {
+      asmCode += "@SP\n"  // Initialize local variables to 0
+                 + "A=M\n" + "M=0\n" + "@SP\n" + "M=M+1\n";
     }
-    return asm_code;
+    return asmCode;
 }
 
 /** Generate Hack Assembly code for a VM call operation */
-string VMTranslator::vm_call(string function_name, int numArgs) {
-    static int callCounter = 0;  // to create unique return labels
-    string returnLabel = "RETURN_LABEL" + to_string(callCounter++);
-
-    string asm_code =
-        "@" + returnLabel + "\n" + "D=A\n" + "@SP\n" + "A=M\n" +
-        "M=D\n" +  // push return address
-        "@SP\n" + "M=M+1\n" +
-
-        "@LCL\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" +  // push LCL
-        "@SP\n" + "M=M+1\n" +
-
-        "@ARG\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" +  // push ARG
-        "@SP\n" + "M=M+1\n" +
-
-        "@THIS\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" +  // push THIS
-        "@SP\n" + "M=M+1\n" +
-
-        "@THAT\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" +  // push THAT
-        "@SP\n" + "M=M+1\n" +
-
-        "@SP\n" + "D=M\n" + "@5\n" + "D=D-A\n" + "@" + to_string(numArgs) +
-        "\n" + "D=D-A\n" + "@ARG\n" + "M=D\n" +  // reposition ARG
-
-        "@SP\n" + "D=M\n" + "@LCL\n" + "M=D\n" +  // reposition LCL
-
-        "@" + function_name + "\n" + "0;JMP\n" +  // goto f
-
-        "(" + returnLabel + ")\n";  // (return-label)
-    return asm_code;
+string VMTranslator::vm_call(string functionName, int numArgs) {
+    string returnLabel = "RETURN_LABEL" + to_string(label_count++);
+    string asmCode = "@" + returnLabel + "\n" + "D=A\n" + "@SP\n" + "A=M\n" +
+                     "M=D\n"  // push return address
+                     + "@SP\n" + "M=M+1\n" + "@LCL\n" + "D=M\n" + "@SP\n" +
+                     "A=M\n" + "M=D\n"  // push LCL
+                     + "@SP\n" + "M=M+1\n" + "@ARG\n" + "D=M\n" + "@SP\n" +
+                     "A=M\n" + "M=D\n"  // push ARG
+                     + "@SP\n" + "M=M+1\n" + "@THIS\n" + "D=M\n" + "@SP\n" +
+                     "A=M\n" + "M=D\n"  // push THIS
+                     + "@SP\n" + "M=M+1\n" + "@THAT\n" + "D=M\n" + "@SP\n" +
+                     "A=M\n" + "M=D\n"  // push THAT
+                     + "@SP\n" + "M=M+1\n" + "@SP\n" + "D=M\n" + "@" +
+                     to_string(numArgs + 5) + "\n" + "D=D-A\n" + "@ARG\n" +
+                     "M=D\n"                                   // ARG = SP-n-5
+                     + "@SP\n" + "D=M\n" + "@LCL\n" + "M=D\n"  // LCL = SP
+                     + "@" + functionName + "\n" + "0;JMP\n"   // goto f
+                     + "(" + returnLabel + ")\n";              // (return-label)
+    return asmCode;
 }
 
 /** Generate Hack Assembly code for a VM return operation */
 string VMTranslator::vm_return() {
-    string asm_code = "@LCL\n" + string("D=M\n") + "@R13\n" + "M=D\n" +  // FRAME = LCL
-
-                      "@5\n" + "A=D-A\n" + "D=M\n" + "@R14\n" +
-                      "M=D\n" +  // RET = *(FRAME-5)
-
-                      "@SP\n" + "A=M-1\n" + "D=M\n" + "@ARG\n" + "A=M\n" +
-                      "M=D\n" +  // *ARG = pop()
-
-                      "@ARG\n" + "D=M+1\n" + "@SP\n" + "M=D\n" +  // SP = ARG+1
-
-                      "@R13\n" + "AM=M-1\n" + "D=M\n" + "@THAT\n" +
-                      "M=D\n" +  // THAT = *(FRAME-1)
-
-                      "@R13\n" + "AM=M-1\n" + "D=M\n" + "@THIS\n" +
-                      "M=D\n" +  // THIS = *(FRAME-2)
-
-                      "@R13\n" + "AM=M-1\n" + "D=M\n" + "@ARG\n" +
-                      "M=D\n" +  // ARG = *(FRAME-3)
-
-                      "@R13\n" + "AM=M-1\n" + "D=M\n" + "@LCL\n" +
-                      "M=D\n" +  // LCL = *(FRAME-4)
-
-                      "@R14\n" + "A=M\n" + "0;JMP\n";  // goto RET
-    return asm_code;
+    string asmCode = "@LCL\n" + "D=M\n" + "@R13\n"              // FRAME is R13
+                     + "M=D\n"                                  // FRAME=LCL
+                     + "@5\n" + "A=D-A\n" + "D=M\n" + "@R14\n"  // RET is R14
+                     + "M=D\n"  // RET=*(FRAME-5)
+                     + "@SP\n" + "AM=M-1\n" + "D=M\n" + "@ARG\n" + "A=M\n" +
+                     "M=D\n"                                     // *ARG=pop()
+                     + "@ARG\n" + "D=M+1\n" + "@SP\n" + "M=D\n"  // SP=ARG+1
+                     + "@R13\n"                                  // FRAME is R13
+                     + "AM=M-1\n" + "D=M\n" + "@THAT\n" +
+                     "M=D\n"  // THAT=*(FRAME-1)
+                     + "@R13\n" + "AM=M-1\n" + "D=M\n" + "@THIS\n" +
+                     "M=D\n"  // THIS=*(FRAME-2)
+                     + "@R13\n" + "AM=M-1\n" + "D=M\n" + "@ARG\n" +
+                     "M=D\n"  // ARG=*(FRAME-3)
+                     + "@R13\n" + "AM=M-1\n" + "D=M\n" + "@LCL\n" +
+                     "M=D\n"                 // LCL=*(FRAME-4)
+                     + "@R14\n"              // RET is R14
+                     + "A=M\n" + "0;JMP\n";  // goto RET
+    return asmCode;
 }
