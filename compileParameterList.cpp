d4d4b7eff3843a2369884a1392eef72976fe1113
Felix <a1878510@adelaide.edu.au>
2023-10-19 22:17:27 +1030

    Update Assi7

---
+++ compileParameterList.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <list>
+
+#include "CompilerParser.h"
+#include "Token.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  /* Tokens for:
+    int a, char b
+   */
+  list<Token*> tokens;
+  tokens.push_back(new Token("keyword", "int"));
+  tokens.push_back(new Token("identifier", "a"));
+  tokens.push_back(new Token("symbol", ","));
+  tokens.push_back(new Token("keyword", "char"));
+  tokens.push_back(new Token("identifier", "b"));
+
+  try {
+    CompilerParser parser(tokens);
+    ParseTree* result = parser.compileProgram();
+    if (result != NULL) {
+      cout << result->tostring() << endl;
+    }
+    } catch (ParseException e) {
+        cout << "Error Parsing!" << endl;
+    }
+}
